76db995a63e1fd3f3b43b7ad4633da0f1338e52e
SiuKeung <siu_keung@outlook.com>
2018-07-09 17:07:14 +0800

    完成了practice_7

--- src/practice_7/teacher.js
+++ src/practice_7/teacher.js
@@ -1,1 +1,21 @@
+const Person = require('./person');
 
+class Teacher extends Person{
+    constructor(name, age, clazz){
+        super(name, age);
+        this.klass = clazz;
+    }
+
+    introduce(){
+        return `${super.introduce()} I am a Teacher. I teach ${this.klass === undefined ? 'No Class' : 'Class ' + this.klass.number}.`;
+    }
+
+    introduceWith(student){
+        let teached = false;
+        if(this.klass !== undefined && this.klass.number === student.klass.number)
+            teached = true;
+        return `${super.introduce()} I am a Teacher. ${teached === true ? 'I teach ' + student.name + '.' : 'I don\'t teach ' + student.name + '.'}`;
+    }
+}
+
+module.exports = Teacher;

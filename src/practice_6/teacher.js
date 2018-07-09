b1c8f1397961ec3029f3247d36cfd8cab4219507
SiuKeung <siu_keung@outlook.com>
2018-07-09 16:36:15 +0800

    完成了practice_6

--- src/practice_6/teacher.js
+++ src/practice_6/teacher.js
@@ -1,1 +1,16 @@
 
+
+const Person = require('./person');
+
+class Teacher extends Person{
+    constructor(name, age, clazz){
+        super(name, age);
+        this.klass = clazz;
+    }
+
+    introduce(){
+        return `${super.introduce()} I am a Teacher. I teach ${this.klass === undefined ? 'No Class' : 'Class ' + this.klass}.`;
+    }
+}
+
+module.exports = Teacher;
